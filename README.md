7fcb8741d89b8e203abdc754c6761a4fa8596d0a
tave <yuriy@ssl-mail.com>
2023-07-20 17:09:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# MongoDB k6 extension
+
+K6 extension to perform tests on mongo.
+
+## Currently Supported Commands
+
+- Supports inserting a document.
+- Supports inserting document batch.
+- Supports find a document based on filter.
+- Supports find all documents of a collection.
+- Supports delete first document based on filter.
+- Supports deleting all documents for a specific filter.
+- Supports dropping a collection.
+
+# xk6-mongo
+A k6 extension for interacting with mongoDb while testing.
+
+## Build
+
+To build a custom `k6` binary with this extension, first ensure you have the prerequisites:
+
+- [Go toolchain](https://go101.org/article/go-toolchain.html)
+- Git
+
+1. Download [xk6](https://github.com/grafana/xk6):
+
+    ```bash
+    go install go.k6.io/xk6/cmd/xk6@latest
+    ```
+
+2. [Build the k6 binary](https://github.com/grafana/xk6#command-usage):
+
+    ```bash
+    xk6 build --with  github.com/GhMartingit/xk6-mongo
+    ```
+
+   This will create a k6 binary that includes the xk6-mongo extension in your local folder. This k6 binary can now run a k6 test.
+
+### Development
+To make development a little smoother, use the `Makefile` in the root folder. The default target will format your code, run tests, and create a `k6` binary with your local code rather than from GitHub.
+
+```shell
+git clone git@github.com/GhMartingit/xk6-mongo.git
+cd xk6-mongo
+make build
+```
+
+Using the `k6` binary with `xk6-mongo`, run the k6 test as usual:
+
+```bash
+./k6 run test.js
+
+```
+
+## Examples: 
+
+### Document Insertion Test
+```js
+import xk6_mongo from 'k6/x/mongo';
+
+
+const client = xk6_mongo.newClient('mongodb://localhost:27017');
+export default ()=> {
+
+    let doc = {
+        correlationId: `test--mongodb`,
+        title: 'Perf test experiment',
+        url: 'example.com',
+        locale: 'en',
+        time: `${new Date(Date.now()).toISOString()}`
+    };
+
+    client.insert("testdb", "testcollection", doc);
+}
+
+```
+
---
+++ mongo.go
@@ -0,0 +1,144 @@
+package xk6_mongo
+
+import (
+	"context"
+	"log"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	k6modules "go.k6.io/k6/js/modules"
+)
+
+// Register the extension on module initialization, available to
+// import from JS as "k6/x/mongo".
+func init() {
+	k6modules.Register("k6/x/mongo", new(Mongo))
+}
+
+// Mongo is the k6 extension for a Mongo client.
+type Mongo struct{}
+
+// Client is the Mongo client wrapper.
+type Client struct {
+	client *mongo.Client
+}
+
+// NewClient represents the Client constructor (i.e. `new mongo.Client()`) and
+// returns a new Mongo client object.
+// connURI -> mongodb://username:password@address:port/db?connect=direct
+func (*Mongo) NewClient(connURI string) interface{} {
+
+	clientOptions := options.Client().ApplyURI(connURI)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return err
+	}
+
+		return  &Client{client: client}
+}
+const filter_is string  = "filter is ";
+
+func (c *Client) Insert(database string, collection string, doc map[string]string) error {
+	db := c.client.Database(database)
+	col := db.Collection(collection)
+	_, err := col.InsertOne(context.TODO(), doc)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func (c *Client) InsertMany(database string, collection string, docs []any) error {
+    log.Printf("Insert multiple documents")
+	db := c.client.Database(database)
+	col := db.Collection(collection)
+	_, err := col.InsertMany(context.TODO(), docs)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func (c *Client) Find(database string, collection string, filter interface{}) []bson.M{
+	db := c.client.Database(database)
+	col := db.Collection(collection)
+	log.Print(filter_is, filter)
+	cur, err := col.Find(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var results []bson.M
+	if err = cur.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+	return results
+}
+
+func (c *Client) FindOne(database string, collection string, filter map[string]string) error {
+	db := c.client.Database(database)
+	col := db.Collection(collection)
+	var result bson.M
+	opts := options.FindOne().SetSort(bson.D{{"_id", 1}})
+	log.Print(filter_is, filter)
+	err := col.FindOne(context.TODO(), filter, opts).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("found document %v", result)
+	return nil
+}
+
+func (c *Client) FindAll(database string, collection string) []bson.M{
+    log.Printf("Find all documents")
+	db := c.client.Database(database)
+	col := db.Collection(collection)
+	cur, err := col.Find(context.TODO(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var results []bson.M
+	if err = cur.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+	return results
+}
+
+func (c *Client) DeleteOne(database string, collection string, filter map[string]string) error {
+	db := c.client.Database(database)
+	col := db.Collection(collection)
+	opts := options.Delete().SetHint(bson.D{{"_id", 1}})
+	log.Print(filter_is, filter)
+	result, err := col.DeleteOne(context.TODO(), filter, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Deleted documents %v", result)
+	return nil
+}
+
+func (c *Client) DeleteMany(database string, collection string, filter map[string]string) error {
+	db := c.client.Database(database)
+	col := db.Collection(collection)
+	opts := options.Delete().SetHint(bson.D{{"_id", 1}})
+    log.Print(filter_is, filter)
+	result, err := col.DeleteMany(context.TODO(), filter, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Deleted documents %v", result)
+	return nil
+}
+
+func (c *Client) DropCollection(database string, collection string) error {
+    log.Printf("Delete collection if present")
+	db := c.client.Database(database)
+	col := db.Collection(collection)
+	err := col.Drop(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
